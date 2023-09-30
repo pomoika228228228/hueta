94f5e7b7b23e405b0980b678ec7b8b80b1fff732
pomoika228228228 <144513372+pomoika228228228@users.noreply.github.com>
2023-09-30 16:38:14 +1000

    blat hueta kokaito

---
+++ main.py
@@ -0,0 +1,40 @@
+print("hyj7u;ewsrdftgyhuj ihgjdhgfj sigfh agjsdhkjghnlkfxjijvkl ghiy fhdskljf8synhfgyewhFK; N")
+# num = 10
+
+# age1 = int(input("Возраст 1"))
+# age2 = int(input("Возраст 2"))
+# age3 = int(input("Возраст 3"))
+
+# if (age1 > age2 and age3 > age2):
+#     print("какого?")
+# elif(age1 < age2 and age2 > age3):
+#     print("ладyшки")
+# else:
+#     print("ну вроде правильно")
+
+
+many = int(input("чё по дентгам?"))
+
+СНОУБОРД = 4999
+СУМОЧКА = 499
+СКОВОРОДКА = 249
+НЕПРИДУМАЛ = 9999
+
+#hui = int(input("чё купиш?"))
+if (many > СКОВОРОДКА and many > СУМОЧКА and many > СНОУБОРД and many > НЕПРИДУМАЛ):
+    print ("тебе хвотает на всё")
+elif(many > СКОВОРОДКА and many > СУМОЧКА and many > СНОУБОРД and many < НЕПРИДУМАЛ):
+    print ("тебе хвотает на сковородку и сумочку и сноуборд")
+elif(many > СКОВОРОДКА and many > СУМОЧКА and many < СНОУБОРД):
+    print ("тебе хвотает на сковородку и сумочку")
+elif(many > СКОВОРОДКА and many < СУМОЧКА ):
+    print ("тебе хватает онли на сковородку")
+elif(many < СКОВОРОДКА ):
+    print ("проёбывай от сюда нищеброд")
+
+
+
+
+
+
+
