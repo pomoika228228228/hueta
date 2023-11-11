462727012954d7d5fec71c2342fa6d07f5eabf8d
pomoika228228228 <144513372+pomoika228228228@users.noreply.github.com>
2023-11-11 16:34:26 +1000

    ЯУКП

---
+++ cobak.py
@@ -0,0 +1,25 @@
+# n = int(input("ВВедите цифорку"))
+
+# if n % 2 == 0 :
+#     print("ЧЁт")
+# else:
+#     print('НЕчёт')
+
+
+
+# vos = int(input("ВВедите свои годики"))
+
+# if vos<=21:
+#     print(vos/10.5)
+# else:
+#     print((vos-21)/4+2)
+
+
+
+age = int(input("xyi"))
+
+if age <=2:
+    age *= 10.5
+else:
+    age = (age -2)*2+21
+print(f"ПО ЧЕЛОВЕЧЕСКИМ МЕРКАМ СОБАКЕ {age} ЛЕТ!!!")
